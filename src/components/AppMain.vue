c71202137923a9ac14b3ef7ed8b4201c8a858e59
DylanBlanco <dylanblanco1198@gmail.com>
2024-11-10 21:58:39 +0100

    richiamato elementi api

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -10,13 +10,13 @@ export default {
     },
     created() {
       axios
-          .get('http://localhost/phpMyAdmin5/index.php?route=/sql&db=classe130_boolfolio&table=projects&pos=0')
+          .get('http://localhost/phpMyAdmin5/index.php?route=/database/structure&server=1&db=classe130_boolfolio')
           .then((res) => {
               console.log('oggetto creato da axios',res);
               console.log('dati che ci ha risposto il server', res.data);
 
               // salva dati
-              this.projects = res.data.data;
+              this.projects = res.data;
           });
     },
     methods: {
@@ -26,16 +26,19 @@ export default {
 
 <template>
     <main>
-        <div class="card mb-3" style="max-width: 540px;">
-            <div class="row g-0">
-                <div class="col-md-4">
-                    <img src="..." class="img-fluid rounded-start" alt="...">
-                </div>
-                <div class="col-md-8">
-                    <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                        <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+        <div class="row p-5">
+            <div class="card m-3" style="max-width: 540px;">
+                <div class="row g-0">
+                    <div class="col-md-4">
+                        <img src="..." class="img-fluid rounded-start" alt="...">
+                    </div>
+                    <div class="col-md-8">
+                        <div v-for="(project, i) in projects" :key="i" class="card-body">
+                            <h5 class="card-title">{{ project.title }}</h5>
+                            <p class="card-text">{{ project.author }}</p>
+                            <p class="card-text">{{ project.description }}</p>
+                            <p class="card-text"><small class="text-body-secondary">{{ project.date_create }}</small></p>
+                        </div>
                     </div>
                 </div>
             </div>
